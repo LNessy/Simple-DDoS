afe6487f1ad55a3367b970dfcb0c9735a746af6a
Nessy <90691149+LNessy@users.noreply.github.com>
2021-09-14 19:20:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import os
 import sys
 import threading
 os.system("cls")
-print("Tools By : Nessy")
 host = input("\nEnter host: ")
 port = int(input("\nEnter port: "))
 print("\n")
+print("Tools By Nessy")
 def run(h):
     while True:
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
