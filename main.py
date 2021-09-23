d09967f19187831005b19004d800ad8cee5f3d9a
Nessy <90691149+LNessy@users.noreply.github.com>
2021-09-23 16:49:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 os.system("cls")
 host = input("\nEnter host: ")
 port = int(input("\nEnter port: "))
-run = input("\nPress Enter To Start "))
+run = input("\nPress Enter To Countinue "))
 print("Tools By Nessy")
 print("Starting Attack....")
 def run(h):
